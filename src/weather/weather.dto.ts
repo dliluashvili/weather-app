72dc743b01a8f31ac010914ff1611a44cbede74a
dato liluashvili <david.liluashvili7@gmail.com>
2022-02-23 15:09:37 +0400

    added validations

--- src/weather/weather.dto.ts
+++ src/weather/weather.dto.ts
@@ -1,5 +1,15 @@
+import { IsDate, IsDateString, IsNotEmpty, IsNumber } from 'class-validator'
+
 export class WeatherDto {
+    @IsNumber()
+    @IsNotEmpty()
     lat: number
+
+    @IsNumber()
+    @IsNotEmpty()
     lng: number
+
+    @IsDateString()
+    @IsNotEmpty()
     date: Date
 }
